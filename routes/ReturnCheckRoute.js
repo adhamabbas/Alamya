f34994a6aacce000108dba9819d97fa31934d4e2
Adham Abbas <57569827+adhamabbas@users.noreply.github.com>
2024-12-18 11:29:52 +0200

    Update ReturnCheckRoute.js

--- routes/ReturnCheckRoute.js
+++ routes/ReturnCheckRoute.js
@@ -4,7 +4,7 @@ const {
     createReturnedCheck,
     getAllReturnedChecks,}= require('../services/ReturnCheckService');
 
-const authService = require('../services/authService');
+//const authService = require('../services/authService');
 
 
 const router = express.Router();
@@ -12,6 +12,6 @@ const router = express.Router();
 router
   .route('/')
   .post(/*authService.protect,authService.allowedTo('admin'),*/createReturnedCheck) // إنشاء شيك مرتجع جديد باستخدام اسم العميل
-  .get(authService.protect,authService.allowedTo('admin'),getAllReturnedChecks); // الحصول على جميع الشيكات المرتجعة
+  .get(/*authService.protect,authService.allowedTo('admin'),*/getAllReturnedChecks); // الحصول على جميع الشيكات المرتجعة
 
 module.exports = router;
