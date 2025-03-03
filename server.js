22fc40f74533c8dbb8caf2260d63aa5cd477319a
Adham Abbas <57569827+adhamabbas@users.noreply.github.com>
2025-03-03 12:58:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
@@ -15,28 +14,28 @@ const dbConnection = require('./config/database');
 // Routes
 const mountRoutes = require('./routes');
 
-
 // Connect with db
 dbConnection();
 
-// express app
+// Express app
 const app = express();
 
 // CORS configuration to allow any origin
 const corsOptions = {
-  origin: '*',
+  origin: '*', // Allow all origins (for development purposes)
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204
 };
 
+// Apply CORS middleware globally
 app.use(cors(corsOptions));
+
+// Handle preflight requests
 app.options('*', cors(corsOptions));
-// compress all responses
+
+// Compress all responses
 app.use(compression());
-
-// Checkout webhook
-
 
 // Middlewares
 app.use(express.json({ limit: '20kb' }));
@@ -44,26 +43,25 @@ app.use(express.static(path.join(__dirname, 'uploads')));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-  console.log(`mode: ${process.env.NODE_ENV}`);
+  console.log(`Mode: ${process.env.NODE_ENV}`);
 }
 
-/*// Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// Limit each IP to 100 requests per `window` (here, per 15 minutes)
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100,
-  message:
-    'Too many accounts created from this IP, please try again after an hour',
+  message: 'Too many requests from this IP, please try again after an hour',
 });
 
 // Apply the rate limiting middleware to all requests
 app.use('/api', limiter);
-*/
 
 // Mount Routes
 mountRoutes(app);
 
+// Handle 404 routes
 app.all('*', (req, res, next) => {
-  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 404));
 });
 
 // Global error handling middleware for express
@@ -71,14 +69,14 @@ app.use(globalError);
 
 const PORT = process.env.PORT || 9900;
 const server = app.listen(PORT, () => {
-  console.log(`App running running on port ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
 
-// Handle rejection outside express
+// Handle unhandled rejections outside express
 process.on('unhandledRejection', (err) => {
   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
-    console.error(`Shutting down....`);
+    console.error('Shutting down...');
     process.exit(1);
   });
 });
